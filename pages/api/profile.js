23059e063d67087749b9262cdc67fb29a822d9f5
Colin Bethea <colinjbethea@gmail.com>
2020-12-31 15:01:35 -0500

    Finish/clean up endpoint

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -2,11 +2,37 @@ import axios from "axios"
 
 export default async (req, res) => {
 
-  // 1: Use Axios to GET data from 'https://pipl.ir/v1/getPerson'
+  const { method } = req
 
-  // 2: Manipulate person's data to get only the data we want
+  const getPerson = async () => {
 
-  // 3: Send both person + (predefined) posts to frontend
+    const person = (await axios.get('https://pipl.ir/v1/getPerson')).data
 
-  res.status(200).json({})
+    const posts = [
+      {
+        title: 'How to Write a Software Engineer Cover Letter',
+        image: 'https://images.unsplash.com/photo-1461681334529-b7210750c909?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2552&q=80'
+      },
+      {
+        title: '5 Pre-IPO Companies to Send Your Resume to in 2020',
+        image: 'https://images.unsplash.com/photo-1607041409099-bd92431680a1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80'
+      },
+      {
+        title: 'Becoming an Engineering Manager in Silicon Valley',
+        image: 'https://images.unsplash.com/photo-1609383645129-c777c3a5c7e4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2551&q=80'
+      }
+    ]
+
+    res.status(200).send({ person, posts })
+  }
+
+
+  switch (method) {
+    case "GET":
+      await getPerson()
+      break
+    default:
+      res.setHeader("Allow", ["GET"])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
 }
